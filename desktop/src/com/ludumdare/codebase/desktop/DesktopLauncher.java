dfa2f92a2a8d3c692ed7196ebb48b835e725ff7a
Hati Eth <hati.eth@gmail.com>
2014-08-22 23:19:39 +0200

    adding html to project

--- desktop/src/com/ludumdare/codebase/desktop/DesktopLauncher.java
+++ desktop/src/com/ludumdare/codebase/desktop/DesktopLauncher.java
@@ -4,18 +4,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ludumdare.codebase.Ludumdare30Main;
 
-public class DesktopLauncher
-{
-    public static void main(String[] arg)
-    {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-
-        config.resizable = true;
-        config.useGL30 = true;
-
-        config.width = 800;
-        config.height = 600;
-
-        new LwjglApplication(new Ludumdare30Main(), config);
-    }
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ludumdare30Main(), config);
+	}
 }
---
+++ html/src/com/ludumdare/codebase/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ludumdare.codebase.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ludumdare.codebase.Ludumdare30Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Ludumdare30Main();
+        }
+}
