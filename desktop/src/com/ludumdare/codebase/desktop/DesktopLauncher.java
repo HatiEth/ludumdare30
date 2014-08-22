20e6dfe5beef78e0a718074c9330d52c191f46e5
Hati Eth <hati.eth@gmail.com>
2014-08-22 22:14:35 +0200

    adding libgdx default project

---
+++ desktop/src/com/ludumdare/codebase/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ludumdare.codebase.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ludumdare.codebase.Ludumdare30;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ludumdare30(), config);
+	}
+}
